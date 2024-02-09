3dbfdd71b428c61a2113656f140e061fa1dfc1ab
JJR <xruzja02@gjk.cz>
2024-02-09 23:34:44 +0100

    test3

--- connection.php
+++ connection.php
@@ -6,9 +6,8 @@ $dbname = "Options";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     }
 catch(PDOException $e)
     {
